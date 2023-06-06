1e6d78f6ce51616ba445a9cc721782aaeca5e321
Priyanshu Ahuja <26710303+Priyanshu-001@users.noreply.github.com>
2023-06-07 00:08:08 +0530

    temp fix: primeicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
         
     //   'primevue/resources/themes/saga-blue/theme.css',
       'primevue/resources/primevue.css',
-      'primeicons/primeicons.css',
+      
       'primeflex/primeflex.css'
     ],
     build: {
